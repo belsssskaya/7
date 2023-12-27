934cf590d3d1210a816fd4fbe5694522e5a2c524
belsssskaya <kate39825@gmail.com>
2023-12-27 10:11:56 +0700

    создание функций вне классов

--- 7/__init__.py
+++ 7/__init__.py
@@ -29,3 +29,39 @@ class DepartmentChair(Human):
 
     def get_info(self):
         return
+
+def add_person(people):
+    print("Adding a new person:")
+    name = input("Enter name: ")
+    age = int(input("Enter age: "))
+    gender = input("Enter gender: ")
+    role = input("Enter role (student/teacher/department chair): ")
+
+    if role == "student":
+        university = input("Enter university: ")
+        faculty = input("Enter faculty: ")
+        course = int(input("Enter course: "))
+        person = Student(name, age, gender, university, faculty, course)
+
+    elif role == "teacher":
+        university = input("Enter university: ")
+        faculty = input("Enter faculty: ")
+        subjects = input("Enter subjects (comma-separated): ").split(", ")
+        person = Teacher(name, age, gender, university, faculty, subjects)
+
+    elif role == "department chair":
+        university = input("Enter university: ")
+        faculty = input("Enter faculty: ")
+        person = DepartmentChair(name, age, gender, university, faculty, [])
+
+    else:
+        print("Invalid role!")
+        return
+
+    people.append(person)
+    print("Person added successfully!\n")
+
+
+def show_people(people):
+    print("People in the registry:")
+    return

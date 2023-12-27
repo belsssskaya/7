060d58756d34a27b6ba28ba379416be057fc619b
belsssskaya <kate39825@gmail.com>
2023-12-27 10:08:47 +0700

    структура классов

---
+++ 7/__init__.py
@@ -0,0 +1,31 @@
+class Human:
+    def __init__(self):
+        return
+
+
+    def get_name(self):
+        return
+
+    def change_gender(self, new_gender):
+        pass
+
+class Student(Human):
+    def __init__(self, name, age, gender, university, faculty, course):
+       pass
+
+    def get_info(self):
+        return
+
+class Teacher(Human):
+    def __init__(self, name, age, gender, university, faculty, subjects):
+        return
+
+    def get_info(self):
+        return
+
+class DepartmentChair(Human):
+    def __init__(self, name, age, gender, university, faculty, teachers):
+        return
+
+    def get_info(self):
+        return
